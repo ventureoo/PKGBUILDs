ventureoo/PKGBUILDs

commit 9075fa7c8386626cdbaef9038f135d99b0031f36

[firefox-settings] Remove Link previews (#16)

firefox-settings/.SRCINFO
@@ -1,6 +1,6 @@
 pkgbase = firefox-settings
 	pkgdesc = My settings for Firefox browser
-	pkgver = 20250827
+	pkgver = 20250903
 	pkgrel = 1
 	url = https://github.com/ventureoo/PKGBUILDs
 	arch = any
@@ -9,7 +9,7 @@ pkgbase = firefox-settings
 	backup = etc/firefox/policies/policies.json
 	source = pure.js
 	source = policies.json
-	sha256sums = 2e4f9682f49ce8cbfe9141255a26725a55725fb3bda71900d5d8401db92e8cf4
+	sha256sums = c0599309e8cc22d57027651a14f0c0e1626a262e9aac8cae365b21c94b78e783
 	sha256sums = 2f2ea4c5926f0a9235b9560381b4b308990be880feca13d29d9402c7177fa5b4
 
 pkgname = firefox-settings

firefox-settings/PKGBUILD
@@ -1,6 +1,6 @@
 # Maintainer: Vasiliy Stelmachenok <ventureo@yandex.ru>
 pkgname=firefox-settings
-pkgver=20250827
+pkgver=20250903
 pkgrel=1
 pkgdesc='My settings for Firefox browser'
 depends=('firefox' 'xdg-desktop-portal')
@@ -12,7 +12,7 @@ source=(
     policies.json
 )
 sha256sums=(
-    '2e4f9682f49ce8cbfe9141255a26725a55725fb3bda71900d5d8401db92e8cf4'
+    'c0599309e8cc22d57027651a14f0c0e1626a262e9aac8cae365b21c94b78e783'
     '2f2ea4c5926f0a9235b9560381b4b308990be880feca13d29d9402c7177fa5b4'
 )
 

firefox-settings/pure.js
@@ -50,6 +50,7 @@ pref("browser.discovery.enabled", false, locked);
 pref("browser.ml.chat.enabled", false, locked);
 pref("browser.ml.chat.page", false, locked);
 pref("browser.ml.enable", false, locked);
+pref("browser.ml.linkPreview.enabled", false, locked)
 pref("browser.newtabpage.enabled", false, locked);
 pref("browser.newtabpage.activity-stream.feeds.aboutpreferences", false, locked);
 pref("browser.newtabpage.activity-stream.feeds.adsfeed", false, locked);

commit a0f1342fb14546723e814135aeb3eb00bed47136

firefox-settings: Fix missed semicolon
Fixes: 9075fa7c8386626cdbaef9038f135d99b0031f36

Signed-off-by: Vasiliy Stelmachenok <ventureo@cachyos.org>

firefox-settings/.SRCINFO
@@ -1,6 +1,6 @@
 pkgbase = firefox-settings
 	pkgdesc = My settings for Firefox browser
-	pkgver = 20250927
+	pkgver = 20250929
 	pkgrel = 1
 	url = https://github.com/ventureoo/PKGBUILDs
 	arch = any
@@ -9,7 +9,7 @@ pkgbase = firefox-settings
 	backup = etc/firefox/policies/policies.json
 	source = pure.js
 	source = policies.json
-	sha256sums = 32aed1acb811f33c9a6cd204a0e7ad8d7891b97768f8e1a5e4d80b14f2150706
+	sha256sums = c803fcad95b3b89839218e673218f7d1be24ae85b585d7c4517babf6034254c9
 	sha256sums = 2f2ea4c5926f0a9235b9560381b4b308990be880feca13d29d9402c7177fa5b4
 
 pkgname = firefox-settings

firefox-settings/PKGBUILD
@@ -1,6 +1,6 @@
 # Maintainer: Vasiliy Stelmachenok <ventureo@yandex.ru>
 pkgname=firefox-settings
-pkgver=20250927
+pkgver=20250929
 pkgrel=1
 pkgdesc='My settings for Firefox browser'
 depends=('firefox' 'xdg-desktop-portal')
@@ -12,7 +12,7 @@ source=(
     policies.json
 )
 sha256sums=(
-    '32aed1acb811f33c9a6cd204a0e7ad8d7891b97768f8e1a5e4d80b14f2150706'
+    'c803fcad95b3b89839218e673218f7d1be24ae85b585d7c4517babf6034254c9'
     '2f2ea4c5926f0a9235b9560381b4b308990be880feca13d29d9402c7177fa5b4'
 )
 

firefox-settings/pure.js
@@ -67,7 +67,7 @@ pref("browser.ipProtection.variant", "", locked);
 pref("browser.ml.chat.enabled", false, locked);
 pref("browser.ml.chat.page", false, locked);
 pref("browser.ml.enable", false, locked);
-pref("browser.ml.linkPreview.enabled", false, locked)
+pref("browser.ml.linkPreview.enabled", false, locked);
 pref("browser.newtabpage.activity-stream.feeds.aboutpreferences", false, locked);
 pref("browser.newtabpage.activity-stream.feeds.adsfeed", false, locked);
 pref("browser.newtabpage.activity-stream.feeds.discoverystreamfeed", false, locked);